95a7abf73e35d1f8120bc5bc6afbb556f8eb9277
Enrique Cerna <ecerna96@gmail.com>
2019-08-05 22:46:33 +0000

    connected grams with users

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,33 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
-      get :index
-      expect(response).to have_http_status(:success)
+    describe "grams#index action" do
+      it "should successfully show the page" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe "grams#new action" do
-    it "should successfully show the new form" do
-      get :new
-      expect(response).to have_http_status(:success)
+
+    describe "grams#create action" do
+
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
     end
-  end
 
-  describe "grams#create action" do
     it "should successfully create a new gram in our database" do
+      user = User.create(
+      email:                 'fakeuser@gmail.com',
+      password:              'secretPassword',
+      password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
       post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
     it "should properly deal with validation errors" do
+      user = User.create(
+      email:                 'fakeuser@gmail.com',
+      password:              'secretPassword',
+      password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
+      gram_count = Gram.count
       post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
+      expect(gram_count).to eq Gram.count
     end
+
   end
 end
